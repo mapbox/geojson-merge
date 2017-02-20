795039cdaccf82d33f4ed84c0931d88ef4f1c1b2
Tom MacWright <tom@macwright.org>
2017-02-19 21:27:59 -0500

    Fix major bug with indexing.

--- index.js
+++ index.js
@@ -27,7 +27,7 @@ function merge (inputs) {
     for (var i = 0; i < inputs.length; i++) {
         var normalized = normalize(inputs[i]);
         for (var j = 0; j < normalized.features.length; j++) {
-            output.features.push(normalized.features[i]);
+            output.features.push(normalized.features[j]);
         }
     }
     return output;
